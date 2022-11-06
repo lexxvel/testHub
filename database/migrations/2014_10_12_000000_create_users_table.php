923540015a720b918cf4e7e063762f36ee2ca11b
Александр lexxvel <lexxvel@outlook.com>
2022-11-06 13:03:09 +0500

    исправил файл миграции

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,10 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id('User_id');
+            $table->id();
             $table->string('email')->unique();
             $table->string('password');
-            $table->int('User_Role');
+            $table->integer('User_Role');
             $table->rememberToken();
             $table->timestamps();
         });
